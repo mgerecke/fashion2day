67fdaee9d5af042deca30f9a001ff569cdb9d823
mgerecke <mgerecke@web.de>
2011-11-27 15:13:55 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fashion2day::Application.config.secret_token = 'a8bdbf901593aa42611b494edc5c788c300492b2af53094d8bfa986ab942da776bc98ae4c63f1c4e6d07be67a64ea6439e071d94d650acfe401ecd9144fdc640'
